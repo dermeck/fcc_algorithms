2dcbe752f22af6b61fa82886798eba15045590ed
Martin Runge <martinrunge@posteo.de>
2016-04-28 10:01:39 +0200

    palindrome: initial commit

---
+++ basic/js/palindrome.js
@@ -0,0 +1,48 @@
+/**
+ * Return true if the given string is a palindrome. Otherwise, return false.
+ *
+ * A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+ */
+
+function palindrome(str) {
+
+    // to lower
+    str.toLowerCase();
+
+    /*
+     [^a-z0-9] - to Remove non Alpha-Numeric characters
+     \s+ - match any collections of spaces
+     \r?\n|\r - match all new line
+     /gmi - global, multi-line, case insensitive
+     */
+    str = str.replace(/[^a-z0-9 \s+]/g, '').replace(/\s+/g, '');
+
+    console.log(str);
+
+
+    var strLength = str.length;
+
+    // create array
+    var strArray = str.split("");
+
+    for (var i = 0; i < strLength / 2; i++) {
+        var left = strArray[i];
+        var right = strArray[strLength -1 - i];
+
+        if (left !== right) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+// palindrome("eye");
+
+palindrome("A man, a plan, a canal. Panama") // should return true.
+// palindrome("never odd or even") //should return true.
+// palindrome("nope") //should return false.
+// palindrome("almostomla")// should return false.
+palindrome("My age is 0, 0 si ega ym.")// should return true.
+// palindrome("1 eye for of 1 eye.") //should return false.
+// palindrome("0_0 (: /-\ :) 0-0") //should return true.
