1b52ee3e58d95175ab7cdbbc1a3bee1bfa4378f0
Martin Runge <martinrunge@posteo.de>
2016-05-01 15:15:19 +0200

    searchanddestroy: added script

---
+++ basic/js/searchanddestroy.js
@@ -0,0 +1,34 @@
+/**
+ * You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments.
+ * Remove all elements from the initial array that are of the same value as these arguments.
+ **/
+
+function destroyer(arr) {
+    // Remove all the values
+
+    argCount = arguments.length;
+    // console.log(arguments);
+
+    var args = arguments;
+
+    var result = arr.filter(function (value) {
+
+        for (var i = 1; i < argCount; i++) {
+
+            if (args[i] === value) {
+                return false;
+            }
+        }
+        return true;
+    });
+
+    console.log(result);
+
+    return result;
+}
+
+// destroyer([1, 2, 3, 1, 2, 3], 2, 3);// should return [1, 1].
+// destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);// should return [1, 5, 1].
+destroyer([3, 5, 1, 2, 2], 2, 3, 5); //should return [1].
+// destroyer([2, 3, 2, 3], 2, 3); // should return [].
+// destroyer(["tree", "hamburger", 53], "tree", 53); // should return ["hamburger"].
